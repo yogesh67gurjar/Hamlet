1b8517d321588e125ffa75c146ea9a7ae7635fb4
yogesh gurjar <yogesh67gurjar@gmail.com>
2023-06-12 15:27:11 +0530

    like

--- app/src/main/java/com/shubham/hamlet/Adapter/Comment/UserCommentViewAdapter.java
+++ app/src/main/java/com/shubham/hamlet/Adapter/Comment/UserCommentViewAdapter.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.shubham.hamlet.Adapter.Message.MessageAdapter;
 import com.shubham.hamlet.R;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class UserCommentViewAdapter extends RecyclerView.Adapter<UserCommentViewAdapter.MyViewHolder> {
     Context context;
@@ -30,6 +32,18 @@ public class UserCommentViewAdapter extends RecyclerView.Adapter<UserCommentView
 
     @Override
     public void onBindViewHolder(@NonNull UserCommentViewAdapter.MyViewHolder holder, int position) {
+        AtomicBoolean likeComment = new AtomicBoolean(false);
+        holder.btnCommentLike.setOnClickListener(v -> {
+
+            if (!likeComment.get()){
+                holder.btnCommentLike.setImageResource(R.drawable.ic_fill_comment_like);
+                likeComment.set(true);
+            }else {
+                holder.btnCommentLike.setImageResource(R.drawable.ic_comment_like);
+                likeComment.set(false);
+            }
+
+        });
 
     }
 
@@ -39,8 +53,11 @@ public class UserCommentViewAdapter extends RecyclerView.Adapter<UserCommentView
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageButton btnCommentLike;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            btnCommentLike = itemView.findViewById(R.id.btnCommentLike);
         }
     }
 }
